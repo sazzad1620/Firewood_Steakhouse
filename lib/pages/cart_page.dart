cc295986ebf6e8c01056fad5a00f985501be53fb
Your Name <you@example.com>
2025-09-01 00:29:56 +0600

    last commit before gateway

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -3,6 +3,7 @@ import 'package:food_delivery_app/components/my_button.dart';
 import 'package:food_delivery_app/components/my_cart_tile.dart';
 import 'package:food_delivery_app/models/cart_item.dart';
 import 'package:food_delivery_app/models/restaurant.dart';
+import 'package:food_delivery_app/pages/payment_page.dart';
 import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
@@ -83,7 +84,12 @@ class CartPage extends StatelessWidget {
               // button to pay
               MyButton(
                 text: "Go to checkout",
-                onTap: () {},
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const PaymentPage(),
+                  ),
+                ),
               ),
 
               const SizedBox(height: 25),
---
+++ lib/pages/payment_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class PaymentPage extends StatefulWidget {
+  const PaymentPage({super.key});
+
+  @override
+  State<PaymentPage> createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Checkout "),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
